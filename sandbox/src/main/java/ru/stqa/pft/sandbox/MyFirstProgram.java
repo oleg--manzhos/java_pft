76da118ba0a482a3c14009efe8990920a2fdf6be
Olg Manzhos <oleg.manzhos@dataart.com>
2017-03-31 13:47:36 +0200

    Project Structure was reworked
    had local problems with structure

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,9 @@
 package ru.stqa.pft.sandbox;
 
-/**
- * Created by omanzhos on 3/30/2017.
- */
 public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        
     }
 }

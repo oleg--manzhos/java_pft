9943b5e11f38597e4fd64cd326b5a1ed1d5891ba
Olg Manzhos <oleg.manzhos@dataart.com>
2017-04-04 16:42:25 +0200

    Point class and its usage was reworked  according to comments

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,36 +5,39 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
 
-        Point p1 = new Point();
-        Point p2 = new Point();
-        Point p3 = new Point(3.0, 6.0, -2.0, 3.0);
+        Point a = new Point(3.0, 6.0);
+        Point b = new Point(-2.0, 3.0);
 
-        p1.cordX = 3;
-        p1.cordY = 6;
 
-        p2.cordX = -2;
-        p2.cordY = 3;
-
-        double dist = 0;
+//        p1.cordX = 3;
+//        p1.cordY = 6;
+//
+//        p2.cordX = -2;
+//        p2.cordY = 3;
 
         //calculation using method (Zadanie 2.2)
-        System.out.println("Distance between two points using distance (p1, p2) method equals to " + distance(p1, p2));
+        //System.out.println("Distance between two points using distance (p1, p2) method equals to " + distance(p1, p2));
 
         //calculation using function (Zadanie 2.3)
-        dist = Math.sqrt(Math.pow(p2.cordX - p1.cordX, 2) + Math.pow(p2.cordY - p1.cordY, 2));
-        System.out.println("Distance between two points equals to " + dist);
+        //dist = Math.sqrt(Math.pow(p2.cordX - p1.cordX, 2) + Math.pow(p2.cordY - p1.cordY, 2));
+        //System.out.println("Distance between two points equals to " + dist);
 
-        //calculation using method of Point class (Zadanie 2.4)
-        System.out.println("Distance between two points calculated using method from Point class equals to " + Point.dist(p1, p2));
+        /*Calculation according to comments from Igor Lyubin:
 
-        //calculation using method of overloaded constructor from the Point class
-        System.out.println("Distance between two points calculated using parameters of overloaded constructor is equal to " + p3.dist());
+        Class Point should describe one point.
+        Like that:
+
+        Point a = new Point(3.0, 6.0);
+        Point b = new Point(-2.0, 3.0);
+        */
+
+        System.out.println("Distance between a and b points calculated using method from Point class equals to " + Point.dist(a, b));
     }
 
-    public static double distance(Point p1, Point p2) {
-        double dist = 0;
-        dist = Math.sqrt(Math.pow(p2.cordX - p1.cordX, 2) + Math.pow(p2.cordY - p1.cordY, 2));
-
-        return dist;
-    }
+//    public static double distance(Point p1, Point p2) {
+//        double dist = 0;
+//        dist = Math.sqrt(Math.pow(p2.cordX - p1.cordX, 2) + Math.pow(p2.cordY - p1.cordY, 2));
+//
+//        return dist;
+//    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,24 +7,19 @@ public class Point {
 
     double cordX = 0;
     double cordY = 0;
-    double x1, x2, y1, y2;
 
-    Point() {
+    Point(double cordX, double cordY) {
+
+        this.cordX = cordX;
+        this.cordY = cordY;
     }
 
-    Point(double cordX1, double cordY1, double cordX2, double cordY2) {
-
-        x1 = cordX1;
-        x2 = cordX2;
-        y1 = cordY1;
-        y2 = cordY2;
-    }
 
     public static double dist(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p2.cordX - p1.cordX, 2) + Math.pow(p2.cordY - p1.cordY, 2));
     }
 
-    public double dist() {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    }
+//    public double dist() {
+//        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+//    }
 }

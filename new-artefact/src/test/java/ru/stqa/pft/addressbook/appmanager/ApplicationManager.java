858ff55b42335d9f06b6e0e34c39f4583b52602f
Olg Manzhos <oleg.manzhos@dataart.com>
2017-04-13 16:41:30 +0200

    Task #7 is completed
    Tests execution was made for IE, Chrome and FF

--- new-artefact/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ new-artefact/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.model.ContactDataGroup;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -17,9 +20,20 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager (String browser){
+        this.browser = browser;
+    }
 
     public void init() {
-        driver = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            driver = new InternetExplorerDriver();
+        }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost:1234/addressbook/");
         groupHelper = new GroupHelper(driver);

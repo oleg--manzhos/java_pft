649919d6c6a219f4961d44d88fc300c3ca10274e
Olg Manzhos <oleg.manzhos@dataart.com>
2017-04-18 17:05:17 +0200

    Group Modification Tests were updtaed

--- new-artefact/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ new-artefact/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,15 +1,47 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+    private final String id;
     private final String name;
     private final String header;
     private final String footer;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (!id.equals(groupData.id)) return false;
+        return name.equals(groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
     public GroupData(String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
 
     public String getName() {
         return name;
@@ -30,18 +62,4 @@ public class GroupData {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
-    }
 }

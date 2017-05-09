211fca31121c53f8bfa24db0ca02b06b3eb5416c
Olg Manzhos <oleg.manzhos@dataart.com>
2017-05-09 17:35:11 +0200

    lamda added

--- new-artefact/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ new-artefact/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,14 +22,20 @@ public class GroupCreationTest extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        before.add(group);
         int max = 0;
         for (GroupData g: after){
             if (g.getId() > max){
                 max = g.getId();
             }
         }
-        group.setId(max);
+
+        Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+        int max1 = after.stream().max(byId).get().getId();
+        group.setId(max1);
+
+
+
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
